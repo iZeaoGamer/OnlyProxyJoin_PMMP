ac3d31164c11748eaf9b41cf005ce10fc504b9ea
PocketMiner82 <44708000+PocketMiner82@users.noreply.github.com>
2020-03-01 15:43:28 +0100

    Add code

---
+++ src/PocketMiner82/OnlyProxyJoin/Main.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PocketMiner82\OnlyProxyJoin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class Main extends PluginBase implements Listener {
+	public function onLoad() {
+		$this->getLogger()->info(Color::YELLOW . "Plugin wird geladen...");
+		$this->saveDefaultConfig();
+	}
+	
+	public function onEnable() {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->getLogger()->info(Color::GREEN . "Plugin wurde aktiviert!");
+	}
+	
+	public function onPreLogin(PlayerPreLoginEvent $ev) {
+		if ($ev->getPlayer()->getAddress() != $this->getConfig()->get("proxyAdress")) {
+			$ev->setKickMessage($this->getConfig()->get("kickMessage"));
+			$ev->setCancelled();
+		}
+	}
+	
+	public function onDisable() {
+		$this->getLogger()->info(Color::RED . "Plugin wurde deaktiviert!");
+	}
+}
